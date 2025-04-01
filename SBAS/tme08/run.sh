f452fed61d9900f54e84f11dac09090563d56c18
Elise Nicolas <elise.nicolas@protonmail.com>
2025-04-01 17:51:01 +0200

    tme8 sbas

---
+++ SBAS/tme08/run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+ALIGNMENT_DIR="/home/elise/Documents/M1-BIM/S2/TMEs/SBAS/tme08/scop/scop/scop95/aln"
+OUTPUT_DIR="/home/elise/Documents/M1-BIM/S2/TMEs/SBAS/tme08/models"
+
+mkdir -p "$OUTPUT_DIR"
+
+for aln_file in "$ALIGNMENT_DIR"/*.sto; do
+    model_name=$(basename "$aln_file" .sto).hmm
+    model_path="$OUTPUT_DIR/$model_name"
+    
+    echo "Running: hmmbuild $model_path $aln_file"
+    hmmbuild "$model_path" "$aln_file"
+done
+
+echo "HMM models have been built successfully."
---
+++ SBAS/tme08/search.sh
@@ -0,0 +1,13 @@
+MODELS_DIR="/home/elise/Documents/M1-BIM/S2/TMEs/SBAS/tme08/models"
+SCOP_TEST="/home/elise/Documents/M1-BIM/S2/TMEs/SBAS/tme08/scop/scop/scopTestSeq.fasta"
+OUTPUT_DIR="/home/elise/Documents/M1-BIM/S2/TMEs/SBAS/tme08/searchResults"
+
+mkdir -p "$OUTPUT_DIR"
+
+for model_file in "$MODELS_DIR"/*.hmm; do
+    tab_name=$(basename "$model_file" .hmm).out
+    echo "Running: hmmsearch "$tab_name $model_file $SCOP_TEST""
+    hmmsearch --domtblout "$OUTPUT_DIR/$tab_name" -E 1 "$model_file" "$SCOP_TEST"
+done
+
+echo "HMM models have been searched successfully."
